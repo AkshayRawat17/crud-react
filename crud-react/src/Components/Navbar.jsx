9e26ca7ab7d14e2b5c0c1f85c143a9bfa05df79d
AkshayRawat17 <rawataksahy603@gmail.com>
2025-05-20 11:52:59 +0530

    second commit

---
+++ crud-react/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+            <ul className='navbar'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </>
+    )
+}
